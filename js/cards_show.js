50706f10d2d1daabc26f7a71e26a8d7d42485123
michele <mikbin80@gmail.com>
2019-12-30 23:44:34 +0100

    layout switcher

--- js/cards_show.js
+++ js/cards_show.js
@@ -1,29 +1,45 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
-  let spades = createCardDomElements("spades", $,true);
-  let diams  =createCardDomElements("diams", $,true);
-  let hearts = createCardDomElements("hearts", $,true);
-  let clubs=createCardDomElements("clubs", $,true);
-  let deck = [...spades,...diams,...hearts,...clubs];
-  let jandb =getJokerAndBackFace($);
-  jandb.forEach(C=>C.css({
-    width:"15.7vh",
-    height:"22vh",
-    "fontSize":"0.4vmin",
-    float:"left"
+  let spades = createCardDomElements("spades", $, true);
+  let diams = createCardDomElements("diams", $, true);
+  let hearts = createCardDomElements("hearts", $, true);
+  let clubs = createCardDomElements("clubs", $, true);
+  let deck = [...spades, ...diams, ...hearts, ...clubs];
+  let jandb = getJokerAndBackFace($);
+  jandb.forEach(C => C.css({
+    width: "15.7vh",
+    height: "22vh",
+    "fontSize": "0.4vmin",
+    float: "left"
   }));
-//style="width:15.7vh; height:22vh; font-size:0.4vmin; float:left"
+  //style="width:15.7vh; height:22vh; font-size:0.4vmin; float:left"
   let $fulldecks = $(".fulldeck");
-let $d0 = $($fulldecks[0]);
-spades.forEach($s=>$d0.append($s));
+  let $d0 = $($fulldecks[0]);
+  spades.forEach($s => $d0.append($s));
   let $d1 = $($fulldecks[1]);
-  diams.forEach($s=>$d1.append($s));
-let $d2 = $($fulldecks[2]);
-hearts.forEach($s=>$d2.append($s));
-let $d3 = $($fulldecks[3]);
-clubs.forEach($s=>$d3.append($s));
-jandb.forEach((j)=>{
-  $("body").append(j);
-})
+  diams.forEach($s => $d1.append($s));
+  let $d2 = $($fulldecks[2]);
+  hearts.forEach($s => $d2.append($s));
+  let $d3 = $($fulldecks[3]);
+  clubs.forEach($s => $d3.append($s));
+  jandb.forEach((j) => {
+    $("body").append(j);
+
+  })
+
+
+  let $linkCss = $("#stylecss");
+  let $menuA = $("#style-options a");
+  let lastActive;
+  $menuA.click(function (e) {
+    let cssVal = $(this).attr('rel');
+    $("a.active").removeClass("active")
+    $(this).addClass("active")
+    $linkCss.attr('href', cssVal);
+    return false;
+  });
+
+
+
 });
